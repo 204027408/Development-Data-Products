4bc3333877afaab78b80f7f6bc411a24e25039a3
204027408 <andres.guerra@ge.com>
2015-02-12 16:17:46 -0500

    Trial2

--- server.r
+++ server.r
@@ -2,6 +2,7 @@ HeartRate <- function(Age) 220-Age
 shinyServer(
         function(input, output) {
                 output$inputValue <- renderPrint({input$Age})
+                output$oGender <- renderPrint({input$Gender})
                 output$MaxHeartRate <- renderPrint({HeartRate(input$Age)})
         }
 )
--- ui.R
+++ ui.R
@@ -1,18 +1,57 @@
+
+# Version 1. Generic Calcularion wih input
+# library(shiny)
+# shinyUI(pageWithSidebar(
+#         headerPanel("Max Heart Rate"),
+#         sidebarPanel(
+#                 numericInput('Age', 'Age (years)', 37, min = 17, max = 70, step = 5),
+#                 submitButton('Submit')
+#                 ),
+#                 mainPanel(
+#                         h3('Recommendation'),
+#                         h4('You entered'),
+#                         verbatimTextOutput("inputValue"),
+#                         h4('Maximum Heart Rate Recommended is(lpm):'),
+#                         verbatimTextOutput("MaxHeartRate")
+#                 )
+#         )
+# )
+
+# V2. With slider
+# library(shiny)
+# shinyUI(pageWithSidebar(
+#         headerPanel("Max Heart Rate"),
+#         sidebarPanel(
+#                 sliderInput('Age', 'Age (years)', value=37, min = 15, max = 75, step = 1,)
+#         ),
+#         mainPanel(
+#                 h3('Recommendation'),
+#                 h4('You entered'),
+#                 verbatimTextOutput("inputValue"),
+#                 h4('Maximum Heart Rate Recommended is(lpm):'),
+#                 verbatimTextOutput("MaxHeartRate")
+#         )
+# )
+# )
+
+
 library(shiny)
-shinyUI(
-        pageWithSidebar(
-                # Application title
-                headerPanel("Max Heart Rate"),
-                sidebarPanel(
-                        numericInput('Age', 'Age Years', 37, min = 17, max = 70, step = 5),
-                        submitButton('Submit')
-                ),
-                mainPanel(
-                        h3('Results of prediction'),
-                        h4('You entered'),
-                        verbatimTextOutput("inputValue"),
-                        h4('Which resulted in a prediction of '),
-                        verbatimTextOutput("MaxHeartRate")
-                )
+shinyUI(pageWithSidebar(
+        headerPanel("Max Heart Rate"),
+        sidebarPanel(
+                sliderInput('Age', 'Age (years)', value=37, min = 15, max = 75, step = 1,),
+                checkboxGroupInput("Gender", "Checkbox",
+                                   c("Female" = "Female",
+                                     "Male" = "Male"))
+        ),
+        mainPanel(
+                h3('Recommendation'),
+                h4('Your Age'),
+                verbatimTextOutput("inputValue"),
+                h4('Your Gender'),
+                verbatimTextOutput("oGender"),
+                h4('Maximum Heart Rate Recommended in lpm is:'),
+                verbatimTextOutput("MaxHeartRate")
         )
+)
 )
